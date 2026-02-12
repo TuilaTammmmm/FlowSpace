1c5a247be17f96396bdf627f1fb6c7f4d1c8241f
TuilaTammmmm <dragonkiller2k5@gmail.com>
2026-02-12 13:30:09 +0700

    refactor layout and add tab functionality in index.html and script.js; update styles in style.css

---
+++ script.js
@@ -0,0 +1,10 @@
+function openTab(tabName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
+    }
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).classList.add("active");
+}
